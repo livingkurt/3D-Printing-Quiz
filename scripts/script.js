273b31d209d5351b1330876f04e8545479694e04
Kurt LaVacque <lavacquek@icloud.com>
2019-12-14 16:47:25 -0600

    Add comments to the js you currently have to keep it more organized

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 
+//Assign Pathways to Variables
+
 //Text
 var title_h_e = document.getElementById("title_h")
 var welcome_h_e = document.getElementById("welcome_h")
@@ -8,7 +10,6 @@ var description_p_e = document.getElementById("description_p")
 var time_l_e = document.getElementById("time_l")
 var final_score_l_e = document.getElementById("final_score_l")
 var enter_initials_l_e = document.getElementById("enter_initials_l")
-
 
 //Lists
 var multiple_choice_ul_e = document.getElementById("multiple_choice_ul")
@@ -23,14 +24,16 @@ var submit_score_b_e = document.getElementById("submit_score_b")
 var go_back_b_e = document.getElementById("go_back_b")
 var clear_highscores_b_e = document.getElementById("clear_highscores_b")
 
-
-var questions_list = {1 : "What does FDM stand for when it refers to FDM 3D Printing?", 2 : "What part of the printer melts the plastic to be extruded?", 3 : "What is the most common filament type?", 4 : "Do you need a heated bed for all types of filaments?", 5 : "What is the most common nozzle size?"};
-var answer_list = {1 : "Fused Depsoition Model", 2 : "Hotend", 3 : "PLA", 4 : "No", 5 : "0.4 mm"};
-var question_1_choice_list = {1 : "Fused Depsoition Model", 2 : "Formal Development Methodology", 3 : "Finite-Difference Method", 4 : "Fused Deposition Manufacturing"};
-var question_2_choice_list = {1 : "Extruder", 2 : "Hotend", 3 : "PTFE Tube", 4 : "Heated Bed"};
-var question_3_choice_list = {1 : "ABS", 2 : "PETG", 3 : "PLA", 4 : "ASA"};
-var question_4_choice_list = {1 : "Yes", 2 : "No"};
-var question_45choice_list = {1 : "0.25mm", 2 : "0.4mm", 3 : "0.6mm", 4 : "0.8mm"};
+//Questions
+var questions_list = ["What does FDM stand for when it refers to FDM 3D Printing?", "What part of the printer melts the plastic to be extruded?", "What is the most common filament type?", "Do you need a heated bed for all types of filaments?", "What is the most common nozzle size?"];
+//Correct Answers
+var answer_list = ["Fused Depsoition Model", "Hotend", "PLA", "No", "0.4 mm"];
+//Multiple Choice
+var question_1_choice_list = ["Fused Depsoition Model", "Formal Development Methodology", "Finite-Difference Method", "Fused Deposition Manufacturing"];
+var question_2_choice_list = ["Extruder", "Hotend", "PTFE Tube", "Heated Bed"];
+var question_3_choice_list = ["ABS", "PETG", "PLA", "ASA"];
+var question_4_choice_list = ["Yes", "No"];
+var question_4_5choice_list = ["0.25mm", "0.4mm", "0.6mm", "0.8mm"];
 
 //Hide Elements
 answer_i_e.setAttribute("style", "display: none;");
@@ -39,50 +42,45 @@ go_back_b_e.setAttribute("style", "display: none;");
 clear_highscores_b_e.setAttribute("style", "display: none;");
 multiple_choice_ul_e.setAttribute("style", "display: none;");
 enter_initials_l_e.setAttribute("style", "display: none;");
-console.log(title_h_e,welcome_h_e,description_p_e,start_quiz_b_e)
-var question_num = 1
+// console.log(title_h_e,welcome_h_e,description_p_e,start_quiz_b_e)
 
-function start_quiz(event, ){
-    // event.preventDefault();
-    // console.log("hello")
-    
-    title_h_e.innerHTML = questions_list[question_num];
-    // title_h_e.setAttribute("style", "justify-content: left");
-    welcome_h_e.setAttribute("style", "display: none;");
-    description_p_e.setAttribute("style", "display: none;");
-    start_quiz_b_e.setAttribute("style", "display: none;");
-    // multiple_choice_ul_e.setAttribute("style", "display: block;");
-    // answer_i_e.setAttribute("style", "display: block;");
-    var li_e = document.createElement("li");
-    li_e.innerHTML = "li";
-    // document.body.appendChild(ul_e);
-    question_num++
-    // 
 
-}
+//Start Question count from 0
+var question_num = 0
 
+//Event Listeners
+start_quiz_b_e.addEventListener("click", start_quiz)
+view_highscores_b_e.addEventListener("click", high_scores)
 document.addEventListener("keypress", function (e) {
     if (e.key === 'Enter') {
         console.log("enter")
         next_question();
-        // code for enter
       }
 })
-start_quiz_b_e.addEventListener("click", start_quiz)
-// document.addEventListener("enter", next_question)
-
-view_highscores_b_e.addEventListener("click", high_scores)
 
 
+//Functions
+
+//Landing Page
+function start_quiz(event){
+    welcome_h_e.setAttribute("style", "display: none;");
+    description_p_e.setAttribute("style", "display: none;");
+    start_quiz_b_e.setAttribute("style", "display: none;");
+    var li_e = document.createElement("li");
+    li_e.innerHTML = "li";
+    question_num++
+
+}
+//Questions
 function next_question() {
     title_h_e.innerHTML = questions_list[question_num];
     question_num++
-    if (question_num > 6){
+    if (question_num > 5){
         all_done();
     }
     
 }
-
+//End Quiz, allows you to type in your initials and submit your score to be saved to local storage
 function all_done() {
     // console.log("enter_name")
     title_h_e.innerHTML = "All Done!"
@@ -96,28 +94,27 @@ function all_done() {
 
 }
 
-
-
+//Shows Highscores page from local storage
 function high_scores() {
     title_h_e.innerHTML = "High Scores";
     // submit_score_b_e.setAttribute("style", "display: block;");
     
 }
-
+//Submits highscore to be stored in local storage and displayed on highscores page
 function submit_highscore() {
     
 }
-
+//Deletes highscores from local storage
 function clear_highscores() {
     title_h_e.innerHTML = "High Scores";
     
 }
-
+//Starts the quiz back at the Landing page
 function start_over() {
     location.reload();
     
 }
-
+//Pops up with a correct or incorrect label
 function correct(){
 
 }
