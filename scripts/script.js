4a8d2987d350660c9ef61aaca1f2abe8bb207d3d
Kurt LaVacque <lavacquek@icloud.com>
2019-12-13 22:35:03 -0600

    Created a click event on the start quiz button, which removes all of the elements except the title, then changes the name of it to a question, working on getting the question from the object that was created too

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,47 @@
-var header_1_e = document.getElementById("header_1")
-var header_2_e = document.getElementById("header_2")
-var description_e = document.getElementById("description_p")
 
+//Text
+var title_h_e = document.getElementById("title_h")
+var welcome_h_e = document.getElementById("welcome_h")
+var description_p_e = document.getElementById("description_p")
+var time_l_e = document.getElementById("time_l")
+var multiple_choice_ul_e = document.getElementById("multiple_choice_ul")
+var answer_input_e = document.getElementById("answer_input")
+
+
+//Buttons
+var start_quiz_b_e = document.getElementById("start_quiz_b")
+
+var view_highscores_b_e = document.getElementById("view_highscores_b")
+
+// var questions_list = {1 : "What does FDM stand for when it refers to FDM 3D Printing?", 2 : "What part of the printer metls the plastic to be extruded?", 3 : "What is the most common filament type?", 4 : "Do you need a heated bed for all types of filaments?", 5 : "What is the most common nozzle size?"};
+// var answer_list = {1 : "Fused Depsoition Model", 2 : "Hotend", 3 : "PLA", 4 : "No", 5 : "0.4 mm"};
+
+
+answer_input_e.setAttribute("style", "display: none;");
+multiple_choice_ul_e.setAttribute("style", "display: none;");
+console.log(title_h_e,welcome_h_e,description_p_e,start_quiz_b_e)
+var question_num = 0
+
+function start_quiz(event){
+    event.preventDefault();
+    // console.log("hello")
+    question_num++
+    title_h_e.innerHTML = "What does FDM stand for when it refers to FDM 3D Printing?";
+    // title_h_e.setAttribute("style", "justify-content: left");
+    welcome_h_e.setAttribute("style", "display: none;");
+    description_p_e.setAttribute("style", "display: none;");
+    start_quiz_b_e.setAttribute("style", "display: none;");
+    // multiple_choice_ul_e.setAttribute("style", "display: block;");
+    answer_input_e.setAttribute("style", "display: block;");
+    var li_e = document.createElement("li");
+    li_e.innerHTML = "li";
+    document.body.appendChild(ul_e);
+    // 
+
+}
+
+start_quiz_b_e.addEventListener("click", start_quiz)
+
+function correct(){
+
+}
